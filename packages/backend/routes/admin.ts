745ac01c055aa6e4edf99ea30cdbdb6f2e309008
avelous <avelousujiri@gmail.com>
2023-10-23 10:33:54 +0100

    added admin routes

--- packages/backend/routes/admin.ts
+++ packages/backend/routes/admin.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { createGame } from "../controllers/Admin";
+import { createGame, changeGameMode, pauseGame, resumeGame, kickPlayer } from "../controllers/Admin";
+import { verifyToken } from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/create", createGame);
+router.patch("/changemode/:id", verifyToken, changeGameMode);
+router.patch("/pause/:id", verifyToken, pauseGame);
+router.patch("/resume/:id", verifyToken, resumeGame);
+router.patch("/kickplayer/:id", verifyToken, kickPlayer);
 
 export default router;
