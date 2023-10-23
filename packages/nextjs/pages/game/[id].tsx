bf54458902405d053d6c8858909c3e66b83f7789
avelous <avelousujiri@gmail.com>
2023-10-23 10:32:04 +0100

    added player and admin roles, ui updates and admin functions

--- packages/nextjs/pages/game/[id].tsx
+++ packages/nextjs/pages/game/[id].tsx
@@ -1,15 +1,20 @@
 // pages/game/[id].js
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
+import QRCode from "qrcode.react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import io from "socket.io-client";
 import { useAccount } from "wagmi";
+import { CheckCircleIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import Congrats from "~~/components/Congrats";
-import { loadGameState, updateGameState } from "~~/utils/diceDemo/game";
+import { Address } from "~~/components/scaffold-eth";
 import serverConfig from "~~/server.config";
+import { loadGameState, updateGameState } from "~~/utils/diceDemo/game";
 
 function GamePage() {
   const router = useRouter();
   const { id } = router.query;
-  const serverUrl = serverConfig.isLocal? serverConfig.localUrl : serverConfig.liveUrl
+  const serverUrl = serverConfig.isLocal ? serverConfig.localUrl : serverConfig.liveUrl;
 
   const { token, game: gameState } = loadGameState();
   const { address } = useAccount();
@@ -20,16 +25,19 @@ function GamePage() {
   const [rolledResult, setRolledResult] = useState<string[]>([]);
   const [game, setGame] = useState(gameState);
   const [isOpen, setIsOpen] = useState(true);
+  const [inviteCopied, setInviteCopied] = useState(false);
   const congratulatoryMessage = "Congratulations! You won the game!";
 
+  const [screenwidth, setScreenWidth] = useState(768);
+
   const calculateLength = () => {
-    const maxLength = 300;
+    const maxLength = 200;
     const calculatedLength = Math.max(maxLength - (game?.diceCount - 1) * 3.8, 10);
 
     return calculatedLength;
   };
 
-  // const isAdmin = address == game?.adminAddress;
+  const isAdmin = address == game?.adminAddress;
   const isPlayer = game?.players?.includes(address);
 
   const generateRandomHex = () => {
@@ -46,8 +54,8 @@ function GamePage() {
     for (let index = 0; index < game.diceCount; index++) {
       rolls.push(generateRandomHex());
     }
-    setRolledResult(rolls);
     setIsRolling(false);
+    setRolledResult(rolls);
     if (!rolled) {
       setRolled(true);
     }
@@ -61,7 +69,7 @@ function GamePage() {
   };
 
   const endGame = async () => {
-    const response = await fetch(`${serverUrl}/game/${game._id}`, {
+    await fetch(`${serverUrl}/game/${game._id}`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -69,10 +77,38 @@ function GamePage() {
       },
       body: JSON.stringify({ winner: address }),
     });
-    const endedGame = await response.json();
+  };
 
-    updateGameState(JSON.stringify(endedGame));
-    setGame(endedGame);
+  const toggleMode = async () => {
+    await fetch(`${serverUrl}/admin/changemode/${game._id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ mode: game?.mode == "manual" ? "auto" : "manual" }),
+    });
+  };
+
+  const pauseResumeGame = async () => {
+    await fetch(`${serverUrl}/admin/${game.status == "ongoing" ? "pause" : "resume"}/${game._id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  const kickPlayer = async (playerAddress: string) => {
+    await fetch(`${serverUrl}/admin/kickplayer/${game._id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ playerAddress: playerAddress }),
+    });
   };
 
   useEffect(() => {
@@ -90,36 +126,150 @@ function GamePage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rolledResult]);
 
+  useEffect(() => {
+    const socket = io(serverUrl);
+    socket.on(`gameUpdate_${game?._id}`, updatedGameData => {
+      setGame(updatedGameData);
+      updateGameState(JSON.stringify(updatedGameData));
+    });
+    return () => {
+      socket.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenWidth(window.innerWidth);
+    }
+    const updateScreenSize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", updateScreenSize);
+    return () => {
+      window.removeEventListener("resize", updateScreenSize);
+    };
+  }, []);
+
   if (!game || game == null || id != game.inviteCode) {
-    return <>nuk</>;
+    return <>null</>;
   }
 
   return (
     <div>
-      <div className="mt-10 2xl:px-40 lg:px-20 w-screen px-10">
-        <div className="w-full  grid lg:grid-cols-4 md:grid-cols-2 gap-3">
-          <div className="bg-base-300 p-4 rounded-box drop-shadow-lg">
-            <p>Invite Code: {id}</p>
+      <div className="flex mt-5 flex-col gap-4 xs:w-4/5 xl:w-[50%] w-11/12 mx-auto">
+        <div className="flex lg:flex-wrap md:flex-row flex-col border rounded-xl">
+          <div className="md:w-1/3 border-r">
+            <div className="font-bold py-2 border-b px-4 flex items-center justify-between">
+              <h1 className=" md:text-2xl text-xl upercase tracking-wide ">INFO</h1>
+              <h1>Role: {isAdmin ? "Admin" : "Player"}</h1>
+            </div>
+            <div className="p-4 ">
+              <div className="p-2 bg-base-300 rounded-md">
+                <div className="flex items-center justify-center">
+                  <span>Invite Code: {id}</span>
+                  {inviteCopied ? (
+                    <CheckCircleIcon
+                      className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <CopyToClipboard
+                      text={id?.toString() || ""}
+                      onCopy={() => {
+                        setInviteCopied(true);
+                        setTimeout(() => {
+                          setInviteCopied(false);
+                        }, 800);
+                      }}
+                    >
+                      <DocumentDuplicateIcon
+                        className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
+                        aria-hidden="true"
+                      />
+                    </CopyToClipboard>
+                  )}
+                </div>
+                <div>
+                  <QRCode
+                    value={id?.toString() || ""}
+                    className=" h-full mx-auto mt-2 w-3/4"
+                    level="H"
+                    renderAs="svg"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col items-center gap-2 bg-base-300 mt-2 rounded-md w-full px-4 py-2">
+                <div className="flex gap-2 justify-center">
+                  <span> Status: {game.status}</span>
+                  {isAdmin && (
+                    <input
+                      id="mode-toggle"
+                      type="checkbox"
+                      className="toggle toggle-primary bg-primary tooltip tooltip-bottom tooltip-primary"
+                      data-tip={game?.status == "ongoing" ? "pause" : game?.status == "paused" ? "resume" : ""}
+                      onChange={pauseResumeGame}
+                      checked={game?.status == "ongoing"}
+                    />
+                  )}
+                </div>
+                <div className="flex gap-2 bg-base-200 mt-2 rounded-md w-full px-4 py-2 justify-center">
+                  <span> Mode: {game.mode}</span>
+                  {isAdmin && (
+                    <input
+                      id="mode-toggle"
+                      type="checkbox"
+                      className="toggle toggle-primary bg-primary tooltip tooltip-bottom tooltip-primary"
+                      data-tip={game?.mode == "manual" ? "auto" : "manual"}
+                      onChange={toggleMode}
+                      checked={game?.mode == "manual"}
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="flex gap-2 bg-base-300 mt-2 rounded-md w-full px-4 py-2 font-bold justify-center">
+                Prize: {game.prize} ETH
+              </div>
+              <div className="flex gap-2 bg-base-300 mt-2 rounded-md w-full px-4 py-2 justify-center">
+                Dice count: {game.diceCount}
+              </div>
+              {game.winner && (
+                <div className="flex gap-2 bg-base-300 mt-2 rounded-md w-full px-4 py-2 justify-center">
+                  Winner <Address address={game.winner} />
+                </div>
+              )}
+            </div>
           </div>
-
-          <div className="bg-base-300 p-4 rounded-box drop-shadow-lg">
-            <p>Hidden Characters {JSON.stringify(game?.hiddenChars)}</p>
-          </div>
-          <div className="bg-base-300 p-4 rounded-box drop-shadow-lg">
-            <p>DiceCount: {game.diceCount} </p>
-            <p>Status: {game.status} </p>
-            <p>Mode: {game.mode} </p>
-            <p>Prize: {game.prize} ETH </p>
-          </div>
-          <div className="bg-base-300 p-4 rounded-box drop-shadow-lg overflow-hidden">
-            <>Players</>
-            {game?.players?.map((user: string) => (
-              <p key={user}>{user}</p>
-            ))}
+          <div className="md:w-2/3">
+            <div>
+              <div className="py-2 border-b md:border-t-0 border-t px-4">
+                <h1 className="font-bold md:text-2xl text-xl upercase  tracking-wide md:text-left text-center ">
+                  HIDDEN CHARACTERS
+                </h1>
+              </div>
+              <p className="text-2xl p-4"> {Object.values(game?.hiddenChars).join(" , ")}</p>
+            </div>
+            <div className="py-2 border-b border-t px-4">
+              <h1 className="font-bold md:text-2xl text-xl upercase  tracking-wide md:text-left text-center">
+                PLAYERS
+              </h1>
+            </div>
+            <div className="p-4">
+              {game?.players?.map((player: string) => (
+                <div key={player} className="mb-4 flex justify-between ">
+                  <Address format={screenwidth > 768 ? "long" : "short"} address={player} />
+                  {isAdmin && (
+                    <button className="btn btn-xs btn-error" onClick={() => kickPlayer(player)}>
+                      kick
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         {isPlayer && (
-          <div className="flex flex-col items-center mt-20">
+          <div className="flex flex-col items-center mt-6">
             <button
               className="btn btn-primary px-10"
               onClick={() => {
@@ -129,8 +279,7 @@ function GamePage() {
             >
               Roll
             </button>
-
-            <div className="flex flex-wrap gap-2">
+            <div className="flex flex-wrap justify-center gap-2 mt-8">
               {Object.entries(game.hiddenChars).map(([key], index) => (
                 <div key={key}>
                   {rolled ? (
