9b571146d5029db0b032ab45c30605a2b5ed0867
avelous <avelousujiri@gmail.com>
2024-02-11 10:13:34 +0100

    cors policy fix

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -34,7 +34,7 @@ app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin: "*",
   }),
 );
 
