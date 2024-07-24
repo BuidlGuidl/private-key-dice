22bf4292560aceb44a5c68c480024c37e90061b5
Avelous Ujiri <86206128+Avelous@users.noreply.github.com>
2024-07-24 17:08:57 +0100

    Merge Staging (#51)
    * ui updates (#50)
    
    * removed welcome roll condition
    
    * welcome roll ui background
    
    * ui update

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -42,7 +42,7 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
       >
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="relative flex flex-col flex-1 overflow-hidden">
+          <main>
             <Component {...pageProps} />
           </main>
           <Footer />
